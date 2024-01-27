c6f8718da572d4addf9f99e61ff1c3873045b02a
Rar Zar <yarzarminkhant2003@gmail.com>
2024-01-28 00:59:39 +0630

    array and foreach

--- index.php
+++ index.php
@@ -2,4 +2,8 @@
 
 $name = htmlspecialchars($_GET["name"]);
 
+$students = [
+    'Mg Mg','Aung Aung','Zaw Zaw'
+];
+
 require('./index.view.php');
--- index.view.php
+++ index.view.php
@@ -7,8 +7,22 @@
 </head>
 <body>
     <h1>
-        <?= "Hello" . $name ?>
+        <?= "Hello " . $name ?>
     </h1>
     <!-- http://localhost:8000/?name=RarZar -->
+
+    <!-- First Way -->
+    <ul>
+    <?php foreach ($students as $student) {
+        echo "<li>$student</li>";
+    }?>
+    </ul>
+
+    <!-- Second Way -->
+    <ul>
+    <?php foreach ($students as $student) : ?>
+        <li><?= $student ?></li>
+    <?php endforeach ?>
+    </ul>
 </body>
 </html>
