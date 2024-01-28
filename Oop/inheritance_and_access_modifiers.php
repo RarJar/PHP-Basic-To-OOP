c48aefe332ec8b0752f9c6a09bec21f0e1c3f799
Rar Zar <yarzarminkhant2003@gmail.com>
2024-01-28 20:34:48 +0630

    inheritance-own-variable-of-child-class

--- Oop/inheritance_and_access_modifiers.php
+++ Oop/inheritance_and_access_modifiers.php
@@ -13,8 +13,10 @@ class Car{
 
 class Truck extends Car{
     public $carName;
-    public function __construct($carName) {
+    private $carWheels;
+    public function __construct($carName,$carWheels) {
         $this->carName = $carName;
+        $this->carWheels = $carWheels;
     }
 
     public function getDrive(){
@@ -22,17 +24,17 @@ class Truck extends Car{
     }
 
     public function stop(){
-        echo $this->carName . "is stop";
+        echo $this->carName . $this->carWheels . "is stop";
     }
 
     public static function startEngine(){
-        return new static("Toyota");
+        return new static("Toyota",12);
         // (Or)
         // return new Truck("Toyota");
     }
 }
 
-$truck = new Truck("LightTruck");
+$truck = new Truck("LightTruck",12);
 $truck->getDrive();
 
 Truck::startEngine()->stop();
