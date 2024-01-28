a5a25e413b4ea60c8f79c01e664de665eece2027
Rar Zar <yarzarminkhant2003@gmail.com>
2024-01-28 11:29:37 +0630

    static method

--- Oop/class_and_method.php
+++ Oop/class_and_method.php
@@ -12,13 +12,19 @@ class Car {
         // (or)
         // return new Car();
     }
+
+    static function staticConcept(){
+        return new Car();
+        // can't use $this in static method
+    }
 }
 
 $car = new Car();
 $car->drive();  //because echo
-
 echo $car->stop();  //because return
 
 $car->thisConcept()->drive(); // because $this
+
+Car::staticConcept()->drive(); // because static
 
 ?>
